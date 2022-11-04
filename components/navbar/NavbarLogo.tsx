687a2daa138b175d23435f588a22158c6358bfd7
TomCat <>
2022-11-04 19:34:05 +0800

    Update NavbarLogo.tsx

--- components/navbar/NavbarLogo.tsx
+++ components/navbar/NavbarLogo.tsx
@@ -15,12 +15,8 @@ type Props = {
 }
 
 const NavbarLogo: FC<Props> = ({ variant, className }) => {
-  const logo = NAVBAR_LOGO || !!DARK_MODE
-  ? `/seaport_watermark_dark.svg`
-  : `/seaport_watermark_light.svg`
-  const desktopLogo = DESKTOP_NAVBAR_LOGO || !!DARK_MODE
-  ? `/seaport_watermark_dark.svg`
-  : `/seaport_watermark_light.svg`
+  const logo = NAVBAR_LOGO || '/seaport.svg'
+  const desktopLogo = DESKTOP_NAVBAR_LOGO || '/seaport-desktop.svg'
   const chain = useEnvChain()
   let logoAlt = 'Logo'
 
@@ -39,14 +35,22 @@ const NavbarLogo: FC<Props> = ({ variant, className }) => {
         className={`relative inline-flex flex-none items-center gap-1 ${className}`}
       >
         <img
-          src={logo}
+          src={
+            !!DARK_MODE
+              ? `/seaport_watermark_dark.svg`
+              : `/seaport_watermark_light.svg`
+          }
           alt={logoAlt}
           className={`h-9 w-auto ${!variant ? 'md:hidden' : ''} ${
             desktopVariant ? 'hidden' : ''
           } ${mobileVariant ? 'block' : ''}`}
         />
         <img
-          src={desktopLogo}
+          src={
+            !!DARK_MODE
+              ? `/seaport_watermark_dark.svg`
+              : `/seaport_watermark_light.svg`
+          }
           alt={logoAlt}
           className={`h-9 w-auto md:block ${
             !variant ? 'hidden md:block' : ''
