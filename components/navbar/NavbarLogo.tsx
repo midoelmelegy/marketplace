2def755bdb11ccfc923342222dcb5de504df2720
TomCat <>
2022-11-04 11:59:04 +0800

    Update NavbarLogo.tsx

--- components/navbar/NavbarLogo.tsx
+++ components/navbar/NavbarLogo.tsx
@@ -40,7 +40,7 @@ const NavbarLogo: FC<Props> = ({ variant, className }) => {
             desktopVariant ? 'hidden' : ''
           } ${mobileVariant ? 'block' : ''}`}
         />
-        <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap">SesPort</span>
+        <span className="reservoir-h1 text-center dark:text-white">SesPort</span>
         <img
           src={desktopLogo}
           alt={logoAlt}
@@ -48,7 +48,6 @@ const NavbarLogo: FC<Props> = ({ variant, className }) => {
             !variant ? 'hidden md:block' : ''
           } ${mobileVariant ? 'hidden' : ''} ${desktopVariant ? 'block' : ''}`}
         />
-        <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap">SesPort</span>
         {chain?.testnet && (
           <div
             className={`reservoir-tiny inline rounded-[4px] bg-[#EFC45C] p-1 py-[2px]
