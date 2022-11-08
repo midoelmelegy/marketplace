1cce7667fd33c26b253027219cf775fcf8359906
TomCat <midoelmelegy@gmail.com>
2022-11-08 10:28:06 +0800

    Update useCollections.ts

--- hooks/useCollections.ts
+++ hooks/useCollections.ts
@@ -7,12 +7,12 @@ import useSWRInfinite, { SWRInfiniteKeyLoader } from 'swr/infinite'
 
 const PROXY_API_BASE = process.env.NEXT_PUBLIC_PROXY_API_BASE
 
-type Collections = paths['/collections/v5']['get']['responses']['200']['schema']
+type Collections = paths['/collections/v2']['get']['responses']['200']['schema']
 
 export default function useCollections() {
   const { ref, inView } = useInView()
 
-  const pathname = `${PROXY_API_BASE}/collections/v5`
+  const pathname = `${PROXY_API_BASE}/collections/v2`
 
   const collections = useSWRInfinite<Collections>(
     (index, previousPageData) => getKey(pathname, index, previousPageData),
@@ -33,13 +33,13 @@ const getKey: (
 ) => ReturnType<SWRInfiniteKeyLoader> = (
   pathname: string,
   index: number,
-  previousPageData: paths['/collections/v5']['get']['responses']['200']['schema']
+  previousPageData: paths['/collections/v2']['get']['responses']['200']['schema']
 ) => {
   // Reached the end
   if (previousPageData && previousPageData?.collections?.length === 0)
     return null
 
-  let query: paths['/collections/v3']['get']['parameters']['query'] = {
+  let query: paths['/collections/v2']['get']['parameters']['query'] = {
     limit: 5,
     offset: 0,
     sortBy: '7DayVolume',
