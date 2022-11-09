5382dc6e684f16d48f23dd14011f3bf4a955d58a
TomCat <midoelmelegy@gmail.com>
2022-11-09 16:29:47 +0800

    Fix privacy

--- pages/privacy/index.tsx
+++ pages/privacy/index.tsx
@@ -1,11 +1,11 @@
 import Layout from 'components/Layout'
 import type { NextPage } from 'next'
 
-const Privacy: NextPage = () => {
+const Terms: NextPage = () => {
   return (
     <Layout navbar={{}}>
       <header className="col-span-full mb-12 mt-[66px] px-4 md:mt-40 lg:px-0">
-        <h1 className="reservoir-h1 text-center dark:text-white">Privacy</h1>
+        <h1 className="reservoir-h1 text-center dark:text-white">Privacy Policy</h1>
       </header>
       <main className='col-span-full mx-auto flex flex-col max-w-4xl w-full px-6 '>
         <p>Privacy content</p>
@@ -14,4 +14,4 @@ const Privacy: NextPage = () => {
   )
 }
 
-export default Privacy
+export default Terms
