e2ae6286db4f3c30bb687303ac6c14d991b0797c
TomCat <midoelmelegy@gmail.com>
2022-11-12 22:16:02 +0800

    Update NavbarLogo.tsx

--- components/navbar/NavbarLogo.tsx
+++ components/navbar/NavbarLogo.tsx
@@ -42,8 +42,7 @@ const NavbarLogo: FC<Props> = ({ variant, className }) => {
           } ${mobileVariant ? 'block' : ''}`}
         />
         <img
-          src={desktopLogo !!DARK_MODE ? `/seaport-desktop.svg` : `/seaport-desktop-light.svg`
-          }
+          src={desktopLogo}
           alt={logoAlt}
           className={`h-9 w-auto md:block ${
             !variant ? 'hidden md:block' : ''
