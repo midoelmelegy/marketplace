17ea6a1cd81491c6868fe678d0a63a6833779f23
TomCat <midoelmelegy@gmail.com>
2022-11-21 14:52:19 +0800

    Update index.tsx

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -3,38 +3,8 @@ import Layout from 'components/Layout'
 import React, { FC } from 'react'
 import CollectionsGridFull from 'components/CollectionsGridFull'
 
-const META_TITLE = process.env.NEXT_PUBLIC_META_TITLE
-const META_DESCRIPTION = process.env.NEXT_PUBLIC_META_DESCRIPTION
-const TAGLINE = process.env.NEXT_PUBLIC_TAGLINE
-
-const metadata = {
-  title: (title: string) => <title>{title}</title>,
-  description: (description: string) => (
-    <meta name="description" content={description} />
-  ),
-  tagline: (tagline: string | undefined) => (
-    <>{tagline || 'Explore, collect, and sell NFTs'}</>
-  ),
-  image: (image?: string) => {
-    if (image) {
-      return (
-        <>
-          <meta name="twitter:image" content={image} />
-          <meta name="og:image" content={image} />
-        </>
-      )
-    }
-    return null
-  },
-}
-
 const Explore: FC = () => {
   const collections = useBigCollections()
-
-  const title = META_TITLE && metadata.title(META_TITLE)
-  const description = META_DESCRIPTION && metadata.description(META_DESCRIPTION)
-  const image = metadata.image(META_IMAGE)
-  const tagline = metadata.tagline(TAGLINE)
 
   return (
     <Layout navbar={{}}>
@@ -43,10 +13,16 @@ const Explore: FC = () => {
         {description}
         {image}
       </Head>
-      <header className="col-span-full mb-12 mt-[66px] px-4 md:mt-40 lg:px-0">
-        <h1 className="reservoir-h1 text-center dark:text-white">{tagline}</h1>
-      </header>
-      <CollectionsGridFull collections={collections} />
+      <header className="col-span-full mb-12 mt-[66px] md:mt-12 lg:px-0">
+                  <h1 className="reservoir-h1 text-center dark:text-white"></h1>
+          </header>
+          <div className="col-span-full small-screen-no-padding md:px-16">
+            <div className="mb-9 flex w-full items-center justify-between">
+              <div className="medium-screen-extra-padding reservoir-h1 dark:text-white">
+              Explore collection
+              </div>
+            </div>
+            <CollectionsGridFull collections={collections} />
     </Layout>
   )
 }
