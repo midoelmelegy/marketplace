62141c5aef15e9330827e4add265169a66460189
TomCat <midoelmelegy@gmail.com>
2022-11-21 14:21:17 +0800

    fix

--- hooks/useBigCollections.ts
+++ hooks/useBigCollections.ts
@@ -44,7 +44,7 @@ const getKey: (
   if (previousPageData && previousPageData?.collections?.length === 0)
     return null
 
-  let query: paths['/collections/v2']['get']['parameters']['query'] = {
+  let query: paths['/collections/v5']['get']['parameters']['query'] = {
     limit: 20,
     offset: index * 20,
     sortBy: '1DayVolume',
--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,43 +1,94 @@
 import useBigCollections from 'hooks/useBigCollections'
+import Layout from 'components/Layout'
+import type { InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
-import React, { FC } from 'react'
 import CollectionsGridFull from 'components/CollectionsGridFull'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+// Environment variables
+// For more information about these variables
+// refer to the README.md file on this repository
+// Reference: https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser
+// REQUIRED
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID
+const RESERVOIR_API_BASE = process.env.NEXT_PUBLIC_RESERVOIR_API_BASE
+
+// OPTIONAL
+const RESERVOIR_API_KEY = process.env.NEXT_PUBLIC_RESERVOIR_API_KEY
+const REDIRECT_HOMEPAGE = process.env.NEXT_PUBLIC_REDIRECT_HOMEPAGE
 const META_TITLE = process.env.NEXT_PUBLIC_META_TITLE
 const META_DESCRIPTION = process.env.NEXT_PUBLIC_META_DESCRIPTION
+const META_IMAGE = process.env.NEXT_PUBLIC_META_OG_IMAGE
 const TAGLINE = process.env.NEXT_PUBLIC_TAGLINE
+const COLLECTION = process.env.NEXT_PUBLIC_COLLECTION
 
-const Explore: FC = () => {
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const metadata = {
+  title: (title: string) => <title>{title}</title>,
+  description: (description: string) => (
+    <meta name="description" content={description} />
+  ),
+  tagline: (tagline: string | undefined) => (
+    <>{tagline || 'Explore, collect, and sell NFTs'}</>
+  ),
+  image: (image?: string) => {
+    if (image) {
+      return (
+        <>
+          <meta name="twitter:image" content={image} />
+          <meta name="og:image" content={image} />
+        </>
+      )
+    }
+    return null
+  },
+}
+
+const Explore: NextPage<Props> = ({ fallback }) => {
   const collections = useBigCollections()
+  const router = useRouter()
 
-  const title = META_TITLE ? (
-    <title>{META_TITLE}</title>
-  ) : (
-    <title>
-      Seaport Market | The ultimate NFT marketplace
-    </title>
-  )
-  const description = META_DESCRIPTION ? (
-    <meta name="description" content={META_DESCRIPTION} />
-  ) : (
-    <meta
-      name="description"
-      content="Reservoir Market is an open source NFT marketplace powered by Reservoir"
-    />
-  )
-  const tagline = TAGLINE || 'Discover, buy and sell NFTs'
+  const title = META_TITLE && metadata.title(META_TITLE)
+  const description = META_DESCRIPTION && metadata.description(META_DESCRIPTION)
+  const image = metadata.image(META_IMAGE)
+  const tagline = metadata.tagline(TAGLINE)
+
+  useEffect(() => {
+    if (REDIRECT_HOMEPAGE && COLLECTION) {
+      router.push(`/collections/${COLLECTION}`)
+    }
+  }, [COLLECTION, REDIRECT_HOMEPAGE])
+
+  // Return error page if the API base url or the environment's
+  // chain ID are missing
+  if (!CHAIN_ID) {
+    console.debug({ CHAIN_ID })
+    return <div>There was an error</div>
+  }
+
+  if (REDIRECT_HOMEPAGE && COLLECTION) return null
 
   return (
-    <>
+    <Layout navbar={{}}>
       <Head>
         {title}
         {description}
+        {image}
       </Head>
-      <header className="col-span-full mb-12 mt-[66px] px-4 md:mt-40 lg:px-0">
-        <h1 className="reservoir-h1 text-center dark:text-white">{tagline}</h1>
-      </header>
-      <CollectionsGridFull collections={collections} />
-    </>
+      <header className="col-span-full mb-12 mt-[66px] md:mt-12 lg:px-0">
+                  <h1 className="reservoir-h1 text-center dark:text-white"></h1>
+          </header>
+          <div className="col-span-full small-screen-no-padding md:px-16">
+            <div className="mb-9 flex w-full items-center justify-between">
+              <div className="medium-screen-extra-padding reservoir-h1 dark:text-white">
+              Explore collections
+              </div>
+            </div>
+            <CollectionsGridFull collections={collections} />
+          </div>
+    </Layout>
   )
 }
 
