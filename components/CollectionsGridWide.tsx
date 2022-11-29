80181445e5ad93d6e3bed272f17523fd6b9af6e6
TomCat <midoelmelegy@gmail.com>
2022-11-29 13:27:46 +0800

    Update CollectionsGridWide.tsx

--- components/CollectionsGridWide.tsx
+++ components/CollectionsGridWide.tsx
@@ -80,11 +80,10 @@ const CollectionsGridWide: FC<Props> = ({ collections }) => {
                     <div className="reservoir-subtitle dark:text-white">
                       {collection?.name}
                     </div>
-                    <div className="items-center gap-1">
-                    <span>Floor:
-                    <FormatEth amount={collection?.floorAsk?.price?.amount?.native} />
-                    </span>
-                    </div>
+                    <div className="h-9 w-px bg-gray-300 dark:bg-neutral-600"></div>
+                        <div className="flex items-center gap-1">
+                        <FormatEth amount={collection?.floorAsk?.price?.amount?.native} />
+                        </div>
                   </div>
                 </a>
               </Link>
