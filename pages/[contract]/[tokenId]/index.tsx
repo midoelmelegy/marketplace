3e7756b6be46226f4b7f838951acc35def97c503
TomCat <midoelmelegy@gmail.com>
2022-11-30 12:14:23 +0800

    Update index.tsx

--- pages/[contract]/[tokenId]/index.tsx
+++ pages/[contract]/[tokenId]/index.tsx
@@ -11,7 +11,6 @@ import CollectionInfo from 'components/token/CollectionInfo'
 import Owner from 'components/token/Owner'
 import PriceData from 'components/token/PriceData'
 import TokenMedia from 'components/token/TokenMedia'
-import TokensGridMore from 'components/TokensGridMore'
 import { useEffect, useState } from 'react'
 import { TokenDetails } from 'types/reservoir'
 import {
@@ -195,11 +194,6 @@ const Index: NextPage<Props> = ({ collectionId, tokenDetails }) => {
         <CollectionInfo collection={collection} token={token.token} />
         <TokenInfo token={token.token} />
       </div>
-      <TokensGridMore
-        tokens={tokens}
-        viewRef={refTokens}
-        collectionImage={collection?.image as string}
-      />
     </Layout>
   )
 }
